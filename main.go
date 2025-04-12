d074292898e5fde83c85c3e8f710b30d2153098d
uLuKaiDev <rflaport@gmail.com>
2025-04-12 23:45:20 +0300

    Database implementation
    - Setup a postgresql database service, connected in main()
    - Used sqlc to generate Go code for typesafe sql interaction
    - Seperated functions into seperate files as the complexity is growing
    - cleaned up the respondWithError response, as it was setting the type to json which was not necessary
    - everything in internal/database is generated by sqlc from the sql/queries folder

--- main.go
+++ main.go
@@ -1,24 +1,44 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/uLuKaiDev/Chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	mux := http.NewServeMux()
-	cfg := &apiConfig{}
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
 
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	dbQueries := database.New(dbConn)
+
+	cfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+		db:             dbQueries,
+	}
+
+	mux := http.NewServeMux()
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -28,98 +48,16 @@ func main() {
 
 	//api endpoints
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
-	mux.HandleFunc("/api/validate_chirp", handlerChirpValidate)
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpValidate)
 
 	//admin endpoints
 	mux.HandleFunc("GET /admin/metrics", cfg.handleMetrics)
 	mux.HandleFunc("POST /admin/reset", cfg.resetMetrics)
 
-	log.Printf("Serving files from %s on port %s\n", filepathRoot, port)
+	log.Printf("Serving on port %s\n", port)
 	log.Printf("Open http://localhost:%s in your browser\n", port)
 
 	// Listen and Serve has to be at the end of the main function
 	// This ensures the server is running. The log.fatal is used only when an error occurs.
 	log.Fatal(srv.ListenAndServe())
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-
-	output := fmt.Sprintf(`
-<html>
-  	<body>
-    	<h1>Welcome, Chirpy Admin</h1>
-    	<p>Chirpy has been visited %d times!</p>
-  	</body>
-</html>
-`, cfg.fileserverHits.Load())
-
-	w.Write([]byte(output))
-}
-
-func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
-}
-
-func handlerChirpValidate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	type chirpRequest struct {
-		Body string `json:"body"`
-	}
-
-	type returnVals struct {
-		Valid bool   `json:"valid"`
-		Error string `json:"error,omitempty"`
-	}
-
-	var chirp chirpRequest
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&chirp)
-	if err != nil {
-		log.Printf("Error decoding request: %s", err)
-		http.Error(w, "Bad Request", http.StatusBadRequest)
-		return
-	}
-
-	var resp returnVals
-	if len(chirp.Body) > 140 {
-		resp.Valid = false
-		resp.Error = "Chirp is too long"
-		w.Header().Set("Content-Type", "application/json: charset=utf-8")
-		w.WriteHeader(http.StatusBadRequest)
-	} else {
-		resp.Valid = true
-		resp.Error = ""
-		w.Header().Set("Content-Type", "application/json: charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-	}
-
-	dat, err := json.Marshal(resp)
-	if err != nil {
-		log.Printf("Error marshaling JSON: %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.Write(dat)
 }
